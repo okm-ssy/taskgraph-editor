56efb71bfbb0e1751e113cae7c3a6ef55e08fd9d
Masashi Okita <okita_masashi@uniquevision.co.jp>
2024-12-28 23:40:16 +0900

    show page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import * as path from 'path';
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias:
+    {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
