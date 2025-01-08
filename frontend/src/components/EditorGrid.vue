6a9a1e598d1f1d564a867ee9c11d77d7603e7250
Masashi Okita <okita_masashi@uniquevision.co.jp>
2025-01-08 21:30:14 +0900

    manage with pinia

--- frontend/src/components/EditorGrid.vue
+++ frontend/src/components/EditorGrid.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
 import { computed, ref } from 'vue';
 import { GridLayout, GridItem } from 'vue3-grid-layout-next';
 
 import { useDragItem } from '../hooks/use_drag_item';
+import { useCurrentTasks } from '../hooks/use_task_store';
 
 import EditorTaskCard from './EditorTaskCard.vue';
 import TaskConnection, { type Connection } from './TaskConnection.vue';
@@ -11,14 +13,13 @@ import TaskSource from './TaskSource.vue';
 const contentRef = ref<HTMLElement | null>(null);
 const gridLayoutRef = ref<InstanceType<typeof GridLayout> | null>(null);
 
-const {
-  updateDragElement,
-  handleDrop,
-  editorTasks,
-  layout,
-  colNum,
-  rowHeight,
-} = useDragItem(contentRef, gridLayoutRef);
+const { updateDragElement, handleDrop, colNum, rowHeight } = useDragItem(
+  contentRef,
+  gridLayoutRef,
+);
+
+const editorTaskStore = useCurrentTasks();
+const { layout, editorTasks } = storeToRefs(editorTaskStore);
 
 const connections = computed<Connection[]>(() => {
   if (editorTasks.value.length > 1) {
@@ -56,13 +57,13 @@ const connections = computed<Connection[]>(() => {
         :use-css-transforms="true"
       >
         <grid-item
-          v-for="(item, index) in editorTasks"
-          :key="item.grid.i"
-          :x="item.grid.x"
-          :y="item.grid.y"
-          :w="item.grid.w"
-          :h="item.grid.h"
-          :i="item.grid.i"
+          v-for="(item, index) in layout"
+          :key="item.i"
+          :x="item.x"
+          :y="item.y"
+          :w="item.w"
+          :h="item.h"
+          :i="item.i"
           class="relative"
         >
           <EditorTaskCard v-model="editorTasks[index]" class="h-full" />
