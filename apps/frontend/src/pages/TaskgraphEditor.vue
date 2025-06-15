fb9e0c3949da4e132100ee8432316b549f2ea36b
okm-ssy <okm.s.choco@gmail.com>
2025-06-15 21:55:23 +0900

    change design

--- apps/frontend/src/pages/TaskgraphEditor.vue
+++ apps/frontend/src/pages/TaskgraphEditor.vue
@@ -17,7 +17,7 @@
     <div v-if="!isMinimalHeader">
       <div class="flex items-center gap-4 mb-4">
         <Switcher v-model="currentPage" />
-        <div class="flex items-center gap-2">
+        <div class="flex items-center gap-2 border p-2 rounded-2xl">
           <button
             :class="[
               'px-4 py-2 rounded-md text-base transition-colors',
