ecf2d68f7442bd944147f3da336651c2e52e91e0
okm-ssy <okm.s.choco@gmail.com>
2025-06-15 15:31:16 +0900

    fix type error

--- apps/frontend/src/components/editor/TaskCard.vue
+++ apps/frontend/src/components/editor/TaskCard.vue
@@ -195,7 +195,6 @@ const isDraggingSource = computed(() => {
 <style scoped>
 .line-clamp-2 {
   display: -webkit-box;
-  -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
